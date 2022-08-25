a5a4b78299a41e534bfdfe70e3c50466adb13a97
Yatidiyora <85158228+Yatidiyora@users.noreply.github.com>
2022-08-25 15:52:24 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,12 @@
+package com.prominent;
+
+public class Strings {
+    public static void main(String[] args) {
+        StringBuffer str1=new StringBuffer("Yatin");
+        str1.append(" Diyora");
+        System.out.println(str1);
+        str1.replace(4,5,"h");
+        System.out.println(str1);
+
+    }
+}
